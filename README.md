dcac56adc17f9a4d40d1e870fecbb99e29c12f01
dayan <eam.dayan@gmail.com>
2024-10-18 01:46:58 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Movie Recommendation
 
 https://realpython.com/build-recommendation-engine-collaborative-filtering/
+
+https://medium.com/@ichigo.v.gen12/detailed-explanation-about-collaborative-filtering-eab116e3b28b
+
+
+```python
+df[df.userId > 10].value_counts()
+df['userId'].value_counts() >= 10
+```
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import joblib
+import kagglehub
+import pandas as pd
 
 model = joblib.load('save/recommender.pkl')
-pred = model.predict(15235.0, 862.0)
-print(pred)
+# pred = model.predict(15235.0, 862.0)
+# model.get_neighbors(862.0, k =10)
+
+test_movie_id = model.trainset.to_inner_iid(862.0)
+n = model.get_neighbors(test_movie_id, k =10)
+print(n)
+
+
+df = pd.read_csv("./data/movies.csv", low_memory=False)
+
+for mid in n:
+    raw_id = model.trainset.to_raw_iid(mid)
+    movie = df.loc[df['movieId']==raw_id]
+    print(movie)
